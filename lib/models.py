0314438ff141e13a606393d0dc38d34a9c700279
Matimath <m.macias@student.uw.edu.pl>
2020-02-18 16:14:40 +0100

    adding original model

--- lib/models.py
+++ lib/models.py
@@ -581,3 +581,272 @@ class cgcnn(base_model):
         return x
 
 print("hello world")
+
+
+
+class cgcnn_original(base_model):
+    """
+    Graph CNN which uses the Chebyshev approximation.
+    The following are hyper-parameters of graph convolutional layers.
+    They are lists, which length is equal to the number of gconv layers.
+        F: Number of features.
+        K: List of polynomial orders, i.e. filter sizes or number of hopes.
+        p: Pooling size.
+           Should be 1 (no pooling) or a power of 2 (reduction by 2 at each coarser level).
+           Beware to have coarsened enough.
+    L: List of Graph Laplacians. Size M x M. One per coarsening level.
+    The following are hyper-parameters of fully connected layers.
+    They are lists, which length is equal to the number of fc layers.
+        M: Number of features per sample, i.e. number of hidden neurons.
+           The last layer is the softmax, i.e. M[-1] is the number of classes.
+    
+    The following are choices of implementation for various blocks.
+        filter: filtering operation, e.g. chebyshev5, lanczos2 etc.
+        brelu: bias and relu, e.g. b1relu or b2relu.
+        pool: pooling, e.g. mpool1.
+    
+    Training parameters:
+        num_epochs:    Number of training epochs.
+        learning_rate: Initial learning rate.
+        decay_rate:    Base of exponential decay. No decay with 1.
+        decay_steps:   Number of steps after which the learning rate decays.
+        momentum:      Momentum. 0 indicates no momentum.
+    Regularization parameters:
+        regularization: L2 regularizations of weights and biases.
+        dropout:        Dropout (fc layers): probability to keep hidden neurons. No dropout with 1.
+        batch_size:     Batch size. Must divide evenly into the dataset sizes.
+        eval_frequency: Number of steps between evaluations.
+    Directories:
+        dir_name: Name for directories (summaries and model parameters).
+    """
+    def __init__(self, L, F, K, p, M, filter='chebyshev5', brelu='b1relu', pool='mpool1',
+                num_epochs=20, learning_rate=0.1, decay_rate=0.95, decay_steps=None, momentum=0.9,
+                regularization=0, dropout=0, batch_size=100, eval_frequency=200,
+                dir_name=''):
+        super().__init__()
+        
+        # Verify the consistency w.r.t. the number of layers.
+        assert len(L) >= len(F) == len(K) == len(p)
+        assert np.all(np.array(p) >= 1)
+        p_log2 = np.where(np.array(p) > 1, np.log2(p), 0)
+        assert np.all(np.mod(p_log2, 1) == 0)  # Powers of 2.
+        assert len(L) >= 1 + np.sum(p_log2)  # Enough coarsening levels for pool sizes.
+        
+        # Keep the useful Laplacians only. May be zero.
+        M_0 = L[0].shape[0]
+        j = 0
+        self.L = []
+        for pp in p:
+            self.L.append(L[j])
+            j += int(np.log2(pp)) if pp > 1 else 0
+        L = self.L
+        
+        # Print information about NN architecture.
+        Ngconv = len(p)
+        Nfc = len(M)
+        print('NN architecture')
+        print('  input: M_0 = {}'.format(M_0))
+        for i in range(Ngconv):
+            print('  layer {0}: cgconv{0}'.format(i+1))
+            print('    representation: M_{0} * F_{1} / p_{1} = {2} * {3} / {4} = {5}'.format(
+                    i, i+1, L[i].shape[0], F[i], p[i], L[i].shape[0]*F[i]//p[i]))
+            F_last = F[i-1] if i > 0 else 1
+            print('    weights: F_{0} * F_{1} * K_{1} = {2} * {3} * {4} = {5}'.format(
+                    i, i+1, F_last, F[i], K[i], F_last*F[i]*K[i]))
+            if brelu == 'b1relu':
+                print('    biases: F_{} = {}'.format(i+1, F[i]))
+            elif brelu == 'b2relu':
+                print('    biases: M_{0} * F_{0} = {1} * {2} = {3}'.format(
+                        i+1, L[i].shape[0], F[i], L[i].shape[0]*F[i]))
+        for i in range(Nfc):
+            name = 'logits (softmax)' if i == Nfc-1 else 'fc{}'.format(i+1)
+            print('  layer {}: {}'.format(Ngconv+i+1, name))
+            print('    representation: M_{} = {}'.format(Ngconv+i+1, M[i]))
+            M_last = M[i-1] if i > 0 else M_0 if Ngconv == 0 else L[-1].shape[0] * F[-1] // p[-1]
+            print('    weights: M_{} * M_{} = {} * {} = {}'.format(
+                    Ngconv+i, Ngconv+i+1, M_last, M[i], M_last*M[i]))
+            print('    biases: M_{} = {}'.format(Ngconv+i+1, M[i]))
+        
+        # Store attributes and bind operations.
+        self.L, self.F, self.K, self.p, self.M = L, F, K, p, M
+        self.num_epochs, self.learning_rate = num_epochs, learning_rate
+        self.decay_rate, self.decay_steps, self.momentum = decay_rate, decay_steps, momentum
+        self.regularization, self.dropout = regularization, dropout
+        self.batch_size, self.eval_frequency = batch_size, eval_frequency
+        self.dir_name = dir_name
+        self.filter = getattr(self, filter)
+        self.brelu = getattr(self, brelu)
+        self.pool = getattr(self, pool)
+        
+        # Build the computational graph.
+        self.build_graph(M_0)
+        
+    def filter_in_fourier(self, x, L, Fout, K, U, W):
+        # TODO: N x F x M would avoid the permutations
+        N, M, Fin = x.get_shape()
+        N, M, Fin = int(N), int(M), int(Fin)
+        x = tf.transpose(x, perm=[1, 2, 0])  # M x Fin x N
+        # Transform to Fourier domain
+        x = tf.reshape(x, [M, Fin*N])  # M x Fin*N
+        x = tf.matmul(U, x)  # M x Fin*N
+        x = tf.reshape(x, [M, Fin, N])  # M x Fin x N
+        # Filter
+        x = tf.matmul(W, x)  # for each feature
+        x = tf.transpose(x)  # N x Fout x M
+        x = tf.reshape(x, [N*Fout, M])  # N*Fout x M
+        # Transform back to graph domain
+        x = tf.matmul(x, U)  # N*Fout x M
+        x = tf.reshape(x, [N, Fout, M])  # N x Fout x M
+        return tf.transpose(x, perm=[0, 2, 1])  # N x M x Fout
+
+    def fourier(self, x, L, Fout, K):
+        assert K == L.shape[0]  # artificial but useful to compute number of parameters
+        N, M, Fin = x.get_shape()
+        N, M, Fin = int(N), int(M), int(Fin)
+        # Fourier basis
+        _, U = graph.fourier(L)
+        U = tf.constant(U.T, dtype=tf.float32)
+        # Weights
+        W = self._weight_variable([M, Fout, Fin], regularization=False)
+        return self.filter_in_fourier(x, L, Fout, K, U, W)
+
+    def spline(self, x, L, Fout, K):
+        N, M, Fin = x.get_shape()
+        N, M, Fin = int(N), int(M), int(Fin)
+        # Fourier basis
+        lamb, U = graph.fourier(L)
+        U = tf.constant(U.T, dtype=tf.float32)  # M x M
+        # Spline basis
+        B = bspline_basis(K, lamb, degree=3)  # M x K
+        #B = bspline_basis(K, len(lamb), degree=3)  # M x K
+        B = tf.constant(B, dtype=tf.float32)
+        # Weights
+        W = self._weight_variable([K, Fout*Fin], regularization=False)
+        W = tf.matmul(B, W)  # M x Fout*Fin
+        W = tf.reshape(W, [M, Fout, Fin])
+        return self.filter_in_fourier(x, L, Fout, K, U, W)
+
+    def chebyshev2(self, x, L, Fout, K):
+        """
+        Filtering with Chebyshev interpolation
+        Implementation: numpy.
+        
+        Data: x of size N x M x F
+            N: number of signals
+            M: number of vertices
+            F: number of features per signal per vertex
+        """
+        N, M, Fin = x.get_shape()
+        N, M, Fin = int(N), int(M), int(Fin)
+        # Rescale Laplacian. Copy to not modify the shared L.
+        L = scipy.sparse.csr_matrix(L)
+        L = graph.rescale_L(L, lmax=2)
+        # Transform to Chebyshev basis
+        x = tf.transpose(x, perm=[1, 2, 0])  # M x Fin x N
+        x = tf.reshape(x, [M, Fin*N])  # M x Fin*N
+        def chebyshev(x):
+            return graph.chebyshev(L, x, K)
+        x = tf.py_func(chebyshev, [x], [tf.float32])[0]  # K x M x Fin*N
+        x = tf.reshape(x, [K, M, Fin, N])  # K x M x Fin x N
+        x = tf.transpose(x, perm=[3,1,2,0])  # N x M x Fin x K
+        x = tf.reshape(x, [N*M, Fin*K])  # N*M x Fin*K
+        # Filter: Fin*Fout filters of order K, i.e. one filterbank per feature.
+        W = self._weight_variable([Fin*K, Fout], regularization=False)
+        x = tf.matmul(x, W)  # N*M x Fout
+        return tf.reshape(x, [N, M, Fout])  # N x M x Fout
+
+    def chebyshev5(self, x, L, Fout, K):
+        N, M, Fin = x.get_shape()
+        N, M, Fin = int(N), int(M), int(Fin)
+        # Rescale Laplacian and store as a TF sparse tensor. Copy to not modify the shared L.
+        L = scipy.sparse.csr_matrix(L)
+        L = graph.rescale_L(L, lmax=2)
+        L = L.tocoo()
+        indices = np.column_stack((L.row, L.col))
+        L = tf.SparseTensor(indices, L.data, L.shape)
+        L = tf.sparse_reorder(L)
+        # Transform to Chebyshev basis
+        x0 = tf.transpose(x, perm=[1, 2, 0])  # M x Fin x N
+        x0 = tf.reshape(x0, [M, Fin*N])  # M x Fin*N
+        x = tf.expand_dims(x0, 0)  # 1 x M x Fin*N
+        def concat(x, x_):
+            x_ = tf.expand_dims(x_, 0)  # 1 x M x Fin*N
+            return tf.concat([x, x_], axis=0)  # K x M x Fin*N
+        if K > 1:
+            x1 = tf.sparse_tensor_dense_matmul(L, x0)
+            x = concat(x, x1)
+        for k in range(2, K):
+            x2 = 2 * tf.sparse_tensor_dense_matmul(L, x1) - x0  # M x Fin*N
+            x = concat(x, x2)
+            x0, x1 = x1, x2
+        x = tf.reshape(x, [K, M, Fin, N])  # K x M x Fin x N
+        x = tf.transpose(x, perm=[3,1,2,0])  # N x M x Fin x K
+        x = tf.reshape(x, [N*M, Fin*K])  # N*M x Fin*K
+        # Filter: Fin*Fout filters of order K, i.e. one filterbank per feature pair.
+        W = self._weight_variable([Fin*K, Fout], regularization=False)
+        x = tf.matmul(x, W)  # N*M x Fout
+        return tf.reshape(x, [N, M, Fout])  # N x M x Fout
+
+    def b1relu(self, x):
+        """Bias and ReLU. One bias per filter."""
+        N, M, F = x.get_shape()
+        b = self._bias_variable([1, 1, int(F)], regularization=False)
+        return tf.nn.relu(x + b)
+
+    def b2relu(self, x):
+        """Bias and ReLU. One bias per vertex per filter."""
+        N, M, F = x.get_shape()
+        b = self._bias_variable([1, int(M), int(F)], regularization=False)
+        return tf.nn.relu(x + b)
+
+    def mpool1(self, x, p):
+        """Max pooling of size p. Should be a power of 2."""
+        if p > 1:
+            x = tf.expand_dims(x, 3)  # N x M x F x 1
+            x = tf.nn.max_pool(x, ksize=[1,p,1,1], strides=[1,p,1,1], padding='SAME')
+            #tf.maximum
+            return tf.squeeze(x, [3])  # N x M/p x F
+        else:
+            return x
+
+    def apool1(self, x, p):
+        """Average pooling of size p. Should be a power of 2."""
+        if p > 1:
+            x = tf.expand_dims(x, 3)  # N x M x F x 1
+            x = tf.nn.avg_pool(x, ksize=[1,p,1,1], strides=[1,p,1,1], padding='SAME')
+            return tf.squeeze(x, [3])  # N x M/p x F
+        else:
+            return x
+
+    def fc(self, x, Mout, relu=True):
+        """Fully connected layer with Mout features."""
+        N, Min = x.get_shape()
+        W = self._weight_variable([int(Min), Mout], regularization=True)
+        b = self._bias_variable([Mout], regularization=True)
+        x = tf.matmul(x, W) + b
+        return tf.nn.relu(x) if relu else x
+
+    def _inference(self, x, dropout):
+        # Graph convolutional layers.
+        x = tf.expand_dims(x, 2)  # N x M x F=1
+        for i in range(len(self.p)):
+            with tf.variable_scope('conv{}'.format(i+1)):
+                with tf.name_scope('filter'):
+                    x = self.filter(x, self.L[i], self.F[i], self.K[i])
+                with tf.name_scope('bias_relu'):
+                    x = self.brelu(x)
+                with tf.name_scope('pooling'):
+                    x = self.pool(x, self.p[i])
+        
+        # Fully connected hidden layers.
+        N, M, F = x.get_shape()
+        x = tf.reshape(x, [int(N), int(M*F)])  # N x M
+        for i,M in enumerate(self.M[:-1]):
+            with tf.variable_scope('fc{}'.format(i+1)):
+                x = self.fc(x, M)
+                x = tf.nn.dropout(x, dropout)
+        
+        # Logits linear layer, i.e. softmax without normalization.
+        with tf.variable_scope('logits'):
+            x = self.fc(x, self.M[-1], relu=False)
+        return x
